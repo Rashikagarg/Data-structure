5085cdac9a1305241889d67bf738b2c584109f90
Rashikagarg <73247928+Rashikagarg@users.noreply.github.com>
2021-06-18 23:45:17 +0530

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, a[n];
+    cout << "Enter the size of erray" << endl;
+    cin >> n;
+    cout << "Enter the value of array" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[n];
+        cout << a[n] << endl;
+    }
+    return 0;
+}
