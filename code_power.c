cd931dc47fc3b9ad2a82e6e74ef64c0eaddeba18
Rashikagarg <73247928+Rashikagarg@users.noreply.github.com>
2021-07-01 22:00:40 +0530

    Add files via upload

---
+++ code_power.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int power(int m, int n)
+{
+    if (n == 0)
+        return 1;
+    return power(m, n - 1) * m;
+}
+int main()
+{
+    int r = power(6, 3);
+    printf("%d", r);
+    return 0;
+}
